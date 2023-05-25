73f21d798c2799d686c99ca1fb801847d8df19a7
kirankadariya <128102618+kirankadariya@users.noreply.github.com>
2023-05-25 16:22:24 +0000

    Pending changes exported from your codespace

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,24 @@
  *---------------------------------------------------------------------------------------*/
 
 package com.mycompany.app;
-
+// Creating class name App 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+
+    public static void main(String[] args) //Declaring main method 
+     {
+    
+       // Declaring variables and assigning value 
+        String snack = "FortuneCookies";
+        int number = 1;
+        String fortuneCookies = "You are good enough";
+
+
+        // Displaying message 
+        System.out.println("What is you are snack today?\n "+ "Tamrat: " + snack);
+        System.out.println("How many do you have? \n "+ "Tamrat:"+ number );
+        System.out.println("Why you have one? \n" + " Tamarat: Because it is "+ snack);
+        System.out.println("Hmm, fortuneCookies, "+ fortuneCookies);
+
+        
     }
 }
